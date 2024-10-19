ebfcccd0ecfaf8f2f68ab63a1c9c04d25c5336da
tshion <tenta_shion@yahoo.co.jp>
2024-10-19 14:10:12 +0900

    Kotlin のバージョンアップ

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    alias(libs.plugins.org.jetbrains.kotlin.android).apply(false)
 }
